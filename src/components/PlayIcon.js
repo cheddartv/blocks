86db5c2cb9acb28c569e56dd8d00fd1254f94ff0
Matt Crowe <crowe.matthew5@gmail.com>
2022-03-23 15:20:21 -0400

    update styling for cheddar cards

--- src/components/PlayIcon.js
+++ src/components/PlayIcon.js
@@ -1,22 +1,41 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+import moment from 'moment'
 import Icon from './Icon'
 
-export default ({ standard }) => {
+export default ({ standard, duration, cheddar }) => {
+  const showDuration = cheddar && duration
+  const getWidth = () => {
+    if (showDuration) return 45
+
+    return standard ? 16 : 12
+  }
+
   const styles = StyleSheet.create({
     playIcon: {
       height: standard ? 16 : 12,
-      width: standard ? 16 : 12,
+      width: getWidth(),
+      flexDirection: 'row',
+      justifyContent: showDuration ? 'center' : 'flex-start',
       paddingVertical: standard ? 3 : 1,
       paddingLeft: standard ? 6 : 4.4,
       backgroundColor: '#F0F200',
       borderRadius: 11.52
+    },
+    durationText: {
+      fontSize: 8,
+      marginLeft: 2
     }
   })
+
+  const formatDuration = (duration) => {
+    return moment(duration*1000).format('mm:ss')
+  }
 
   return (
     <View style={styles.playIcon}>
       <Icon name="play" size={standard ? 10 : 9} />
+      {showDuration && <Text style={styles.durationText}>{formatDuration(duration)}</Text>}
     </View>
   )
 }
