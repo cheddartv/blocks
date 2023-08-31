cbe00f8027a8dfda5e8cac9c759ea15590ee350e
Maryna <maryna.humeniuk@sigma.software>
2023-08-31 13:06:15 +0300

    refactor(type): added additional prop for date container

--- src/components/cards/HorizontalCard.js
+++ src/components/cards/HorizontalCard.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
-import { articlePropTypes } from '../../types'
-import { getThumbnailFallback } from '../../utils/getThumbnailFallback'
-import DateContainer from '../DateContainer'
-import theme from '../../theme'
-import PropTypes from 'prop-types'
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { articlePropTypes } from "../../types";
+import { getThumbnailFallback } from "../../utils/getThumbnailFallback";
+import DateContainer from "../DateContainer";
+import theme from "../../theme";
+import PropTypes from "prop-types";
 
 const HorizontalCard = ({ article, cheddar = false, darkMode = false }) => {
   const styles = StyleSheet.create({
     container: {
-      flexDirection: 'row',
+      flexDirection: "row",
       paddingVertical: 12,
       marginHorizontal: 20,
       borderTopWidth: 0.25,
-      borderStyle: 'solid',
-      borderTopColor: theme.colors[darkMode ? 'news12Metallic' : 'lightBlack']
+      borderStyle: "solid",
+      borderTopColor: theme.colors[darkMode ? "news12Metallic" : "lightBlack"],
     },
     title: {
       paddingLeft: 10,
       flexShrink: 1,
-      width: '100%'
+      width: "100%",
     },
     titleText: {
       paddingTop: 6.8,
-      fontFamily: cheddar ? 'Gotham' : 'Graphik-Medium',
-      alignContent: 'center',
-      fontWeight: '500',
+      fontFamily: cheddar ? "Gotham" : "Graphik-Medium",
+      alignContent: "center",
+      fontWeight: "500",
       fontSize: 16,
-      lineHeight: 22
+      lineHeight: 22,
     },
     darkMode: {
-      color: 'white'
-    }
-  })
+      color: "white",
+    },
+  });
 
   return (
     <View style={styles.container}>
@@ -40,7 +40,7 @@ const HorizontalCard = ({ article, cheddar = false, darkMode = false }) => {
         <Image
           style={{ height: 90, width: 90 }}
           source={{
-            uri: article?.thumbnails?.medium || getThumbnailFallback(cheddar)
+            uri: article?.thumbnails?.medium || getThumbnailFallback(cheddar),
           }}
         />
       </View>
@@ -51,16 +51,18 @@ const HorizontalCard = ({ article, cheddar = false, darkMode = false }) => {
           cheddar={cheddar}
           duration={article?.videoFile?.duration}
         />
-        <Text style={[styles.titleText, darkMode && styles.darkMode]}>{article?.title}</Text>
+        <Text style={[styles.titleText, darkMode && styles.darkMode]}>
+          {article?.title}
+        </Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
 export const HorizontalCardPropTypes = {
   ...articlePropTypes,
-  cheddar: PropTypes.bool
-}
-HorizontalCard.propTypes = HorizontalCardPropTypes
+  cheddar: PropTypes.bool,
+};
+HorizontalCard.propTypes = HorizontalCardPropTypes;
 
-export default HorizontalCard
+export default HorizontalCard;
