bbd79db917d5eadbf855dab98882114dfe2bf207
Maryna <maryna.humeniuk@sigma.software>
2023-09-06 12:48:51 +0300

    refactor(formatting): removed semicolons

--- src/components/DateContainer.js
+++ src/components/DateContainer.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import moment from "moment";
-import PropTypes from "prop-types";
-import PlayIcon from "./PlayIcon";
-import theme from "../theme";
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import moment from 'moment'
+import PropTypes from 'prop-types'
+import PlayIcon from './PlayIcon'
+import theme from '../theme'
 
 const renderPlayIcon = (isArticle, isStoryList, cheddar, duration) => {
   const styles = StyleSheet.create({
     icon: {
       marginLeft: 12,
-      justifyContent: "center",
+      justifyContent: 'center',
     },
-  });
+  })
 
   return !isArticle || !isStoryList ? (
     <View style={styles.icon}>
       <PlayIcon standard={true} cheddar={cheddar} duration={duration} />
     </View>
-  ) : null;
-};
+  ) : null
+}
 
 const DateContainer = ({
   publicAt,
@@ -31,41 +31,41 @@ const DateContainer = ({
   darkMode = false,
 }) => {
   const getFontStyle = () => {
-    if (isArticle || cheddar || isStoryList) return "normal";
+    if (isArticle || cheddar || isStoryList) return 'normal'
 
-    return "italic";
-  };
+    return 'italic'
+  }
 
   const getFontColor = () => {
-    if (!isArticle) return theme.colors.news12Grey;
+    if (!isArticle) return theme.colors.news12Grey
 
-    if (isStoryList) return theme.colors.news12Metallic;
+    if (isStoryList) return theme.colors.news12Metallic
 
-    if (darkMode) return "white";
+    if (darkMode) return 'white'
 
-    return theme.colors.lightBlack;
-  };
+    return theme.colors.lightBlack
+  }
 
   const styles = StyleSheet.create({
     dateContainer: {
-      flexDirection: "row",
-      justifyContent: cheddar ? "space-between" : "flex-start",
+      flexDirection: 'row',
+      justifyContent: cheddar ? 'space-between' : 'flex-start',
       marginBottom: cheddar && media ? 10 : 0,
     },
     publicAt: {
-      fontFamily: cheddar ? "Gotham" : "Graphik-MediumItalic",
+      fontFamily: cheddar ? 'Gotham' : 'Graphik-MediumItalic',
       color: getFontColor(),
       fontSize: isArticle || isStoryList ? 15 : 12,
-      fontWeight: isArticle ? "500" : isStoryList ? "800" : "400",
+      fontWeight: isArticle ? '500' : isStoryList ? '800' : '400',
       lineHeight: 21,
       fontStyle: getFontStyle(),
       paddingBottom: isArticle ? 15 : 0,
     },
-  });
+  })
 
   const getDateFormat = () => {
-    return cheddar || isStoryList ? "MMMM DD, YYYY" : "MMMM DD, YYYY h:mm A";
-  };
+    return cheddar || isStoryList ? 'MMMM DD, YYYY' : 'MMMM DD, YYYY h:mm A'
+  }
 
   return (
     <View style={styles.dateContainer}>
@@ -74,8 +74,8 @@ const DateContainer = ({
       </Text>
       {!dontShowIcon && renderPlayIcon(isArticle, cheddar, duration)}
     </View>
-  );
-};
+  )
+}
 
 export const DateContainerPropTypes = {
   publicAt: PropTypes.string,
@@ -85,7 +85,7 @@ export const DateContainerPropTypes = {
   cheddar: PropTypes.bool,
   duration: PropTypes.number,
   media: PropTypes.bool,
-};
-DateContainer.propTypes = DateContainerPropTypes;
+}
+DateContainer.propTypes = DateContainerPropTypes
 
-export default DateContainer;
+export default DateContainer
